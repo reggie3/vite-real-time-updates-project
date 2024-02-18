a53dd60c37f138f2deb75cca49c3550f7aa26412
reggie3 <reggie3@protonmail.com>
2024-02-18 16:04:55 -0500

    not seeing database changes in kafka

--- server/index.ts
+++ server/index.ts
@@ -1,32 +1,103 @@
-import postgres from "postgres";
+import { PrismaClient } from "@prisma/client";
 
-console.log(process.env.DATABASE_USER)
-console.log(process.env.DATABASE_PASSWORD)
-console.log(process.env.DATABASE_NAME)
+const CORS_HEADERS = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET, PUT, DELETE",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  },
+};
 
-const sql = postgres(`postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@localhost:5432/${process.env.DATABASE_NAME}`)
-const [{ version }] = await sql`SELECT version()`;
-
-console.log(version); 
-
+const prisma = new PrismaClient();
 
 Bun.serve({
-    fetch(req) {
-      const url = new URL(req.url);
-      console.log(url.pathname)
-      if (url.pathname === "/") return new Response("Home page!");
-      if (url.pathname === "/todos") return handleTodos(req);
-      return new Response("404!");
-    },
-  });
+  fetch(req) {
+    // Handle CORS preflight requests
+    if (req.method === "OPTIONS") {
+      const res = new Response("Departed", CORS_HEADERS);
+      return res;
+    }
 
+    const url = new URL(req.url);
+    console.log(url.pathname);
+    if (url.pathname === "/") return new Response("Home page!");
+    if (url.pathname === "/todos") return handleTodos(req);
+    if (url.pathname.startsWith("/todos")) {
+      const taskId = url.pathname.split("/")[2];
+      return handleTodoWithId(req, taskId);
+    }
+    return new Response("404!");
+  },
+});
 
-const handleTodos=async (req:Request)=>{
-    if (req.method === "GET") {
-        return await sql`SELECT * FROM todos`;
+const handleTodos = async (req: Request) => {
+  switch (req.method) {
+    case "GET":
+      const todos = await prisma.todo.findMany();
+      return new Response(JSON.stringify({ todos }), CORS_HEADERS);
+
+    case "POST":
+      if (!req?.headers?.get("content-type")?.includes("application/json")) {
+        return new Response("Invalid content type!", { status: 400 });
       }
-      if (req.method === "POST") {
-        return new Response("POST todos!");
+      const body = await req.json();
+      if (!body.title) {
+        return new Response("Title is required!", { status: 400 });
       }
+
+      const { title } = body;
+      const todo = await prisma.todo.create({
+        data: {
+          title,
+          id: Math.random().toString(36).substring(7),
+        },
+      });
+      return new Response(JSON.stringify({ todo }), CORS_HEADERS);
+
+    default:
       return new Response("404!");
-}
+  }
+};
+
+const handleTodoWithId = async (req: Request, todoId: string) => {
+  console.log({ todoId });
+
+  let todo;
+
+  switch (req.method) {
+    case "DELETE":
+      todo = await prisma.todo.delete({
+        where: {
+          id: todoId,
+        },
+      });
+      break;
+
+    case "PUT":
+      if (!req?.headers?.get("content-type")?.includes("application/json")) {
+        return new Response("Invalid content type!", { status: 400 });
+      }
+      const body = await req.json();
+      todo = await prisma.todo.update({
+        where: {
+          id: todoId,
+        },
+        data: body,
+      });
+      break;
+
+    case "GET":
+      todo = await prisma.todo.findUnique({
+        where: {
+          id: todoId,
+        },
+      });
+
+      break;
+
+    default:
+      return new Response("404!");
+  }
+
+  return new Response(JSON.stringify({ todo }), CORS_HEADERS);
+};
